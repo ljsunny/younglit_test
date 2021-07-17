e8f4f6e7c9b81bfd0dc2e58bec26a4652adf29ec
ljsunny <43569683+ljsunny@users.noreply.github.com>
2021-07-18 02:54:10 +0900

    input youtube player
    유튜브 iframe 삽입 및 객체 처리
    : stop video

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 "use strict";
+///////////////////////////////////
+// 유튜브 영상 처리
+//////////////////////////////////
 
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    // height: "360",
+    // width: "640",
+    // videoId: "M7lc1UVf-VE",
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+///////////////////////////////////////////////////////////////////////////////////
+// go_to_page
+//////////////////////////////////////////////////////////////////////////////////
 function go_page(page) {
   const section = document.querySelector(".home__container");
   const nav = document.querySelector(".nav__list");
@@ -9,7 +55,6 @@ function go_page(page) {
     section.style.marginLeft = 0;
 
     active.classList.remove("active");
-    console.log(nav.children[0]);
     const sec = nav.children[0];
     sec.classList.add("active");
   }
@@ -31,4 +76,6 @@ function go_page(page) {
     const sec = nav.children[3];
     sec.classList.add("active");
   }
+
+  stopVideo();
 }
